37cfaa8680808061d6796469d84904be757767f2
Alanrl22 <alan.raoul22@gmail.com>
2020-06-24 17:23:34 +0200

    Creation of constructor, getters and setters of the prescription entity

---
+++ src/main/java/com/Elephants/doctolibElephants/entity/Prescription.java
@@ -0,0 +1,53 @@
+package com.Elephants.doctolibElephants.entity;
+
+public class Prescription {
+
+    private Long id;
+    private String drug;
+    private String takenDay;
+    private String day;
+    private String comment;
+
+    public Prescription() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDrug() {
+        return drug;
+    }
+
+    public void setDrug(String drug) {
+        this.drug = drug;
+    }
+
+    public String getTakenDay() {
+        return takenDay;
+    }
+
+    public void setTakenDay(String takenDay) {
+        this.takenDay = takenDay;
+    }
+
+    public String getDay() {
+        return day;
+    }
+
+    public void setDay(String day) {
+        this.day = day;
+    }
+
+    public String getComment() {
+        return comment;
+    }
+
+    public void setComment(String comment) {
+        this.comment = comment;
+    }
+}
