d5ff5609fdd3d2bc9b761e6fba570df269861639
mroldan747 <mroldan747@gmail.com>
2020-06-24 09:24:08 +0200

    init project

---
+++ src/main/java/com/Elephants/doctolibElephants/DoctolibElephantsApplication.java
@@ -0,0 +1,13 @@
+package com.Elephants.doctolibElephants;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DoctolibElephantsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DoctolibElephantsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/Elephants/doctolibElephants/DoctolibElephantsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.Elephants.doctolibElephants;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DoctolibElephantsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
