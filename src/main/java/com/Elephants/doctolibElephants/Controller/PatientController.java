18c32fe2bc7f7c17fb7288c1885cdf529591c20f
mroldan747 <mroldan747@gmail.com>
2020-06-25 11:55:12 +0200

    medicament dynamic

--- src/main/java/com/Elephants/doctolibElephants/Controller/PatientController.java
+++ src/main/java/com/Elephants/doctolibElephants/Controller/PatientController.java
@@ -1,10 +1,28 @@
 package com.Elephants.doctolibElephants.Controller;
 
+import com.Elephants.doctolibElephants.entity.FollowUp;
+import com.Elephants.doctolibElephants.entity.Ordonnance;
+import com.Elephants.doctolibElephants.entity.Prescription;
+import com.Elephants.doctolibElephants.repository.OrderRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.LinkedHashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 @Controller
 public class PatientController {
+
+    @Autowired
+    OrderRepository orderRepository;
+
 
     @GetMapping("/dashboard/Patient")
     public String dashboardPatient() {
@@ -12,7 +30,51 @@ public class PatientController {
     }
 
     @GetMapping("/medicament")
-    public String medicament() {
+    public String medicament(Model model, @RequestParam Long med, @RequestParam Long id) {
+        Optional<Ordonnance> optionalOrdonnance = orderRepository.findByPatientId(id);
+        if (optionalOrdonnance.isPresent()) {
+            Prescription prescription = optionalOrdonnance.get().getPrescriptions().stream()
+                    .filter(item -> item.getId().equals(med))
+                    .collect(Collectors.toList()).get(0);
+
+            if (prescription.getStartHours() == null) {
+                model.addAttribute("isStart", false);
+            } else {
+                model.addAttribute("isStart", true);
+                List<FollowUp> followUps = prescription.getFollowUps();
+                Map<FollowUp, String> followUpsMap = new LinkedHashMap<>();
+                for (FollowUp followUp : followUps) {
+                    if (followUp.getStatus() == 3) {
+                        followUpsMap.put(followUp, "red");
+                    } else if (followUp.getStatus() == 2) {
+                        followUpsMap.put(followUp, "orange");
+                    } else {
+                        followUpsMap.put(followUp, "green");
+                    }
+                }
+                model.addAttribute("followUps", followUpsMap);
+            }
+
+        } else {
+            model.addAttribute("isStart", false);
+        }
+        model.addAttribute("med", med);
+        model.addAttribute("id", id);
         return "medicament";
     }
+
+    @PostMapping("/medicament")
+    public String startHour(@RequestParam Long med, @RequestParam Long id, @RequestParam Integer hour) {
+        Optional<Ordonnance> optionalOrdonnance = orderRepository.findByPatientId(id);
+        if (optionalOrdonnance.isPresent()) {
+            Prescription prescription = optionalOrdonnance.get().getPrescriptions().stream()
+                    .filter(item -> item.getId().equals(med))
+                    .collect(Collectors.toList()).get(0);
+            prescription.setStartHours(hour);
+        }
+
+        return "redirect:/medicament?med=" + med + "&" + "id=" + id;
+    }
+
+
 }
--- src/main/java/com/Elephants/doctolibElephants/repository/OrderRepository.java
+++ src/main/java/com/Elephants/doctolibElephants/repository/OrderRepository.java
@@ -4,6 +4,9 @@ import com.Elephants.doctolibElephants.entity.Ordonnance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface OrderRepository extends JpaRepository<Ordonnance, Long> {
+    Optional<Ordonnance> findByPatientId(Long id);
 }
